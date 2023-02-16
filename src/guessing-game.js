7a492fecd28881a64b6a6298b05e97ebbfd2beef
arthurchic20 <arthurchic20@gmail.com>
2023-02-17 01:18:43 +0600

    init: start and end project guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    }
+    
     setRange(min, max) {
-
+        this.min = +min;
+        this.max = +max;
     }
-
+    
     guess() {
-
+        return this.res = Math.round((this.min + this.max)/2)
     }
-
+    
     lower() {
-
+        this.max = this.res 
     }
-
+    
     greater() {
-
+        this.min = this.res
     }
 }
 
 module.exports = GuessingGame;
+
+// const game = new GuessingGame();
+// game.setRange(0, 4048)
+
+// console.log(game.setRange(0, 4048))
+// console.log(game.min, game.max)
+// console.log(game.guess())
